78357d239a77ac0215088fd61a0ad79320de77d7
Sahil12345678987 <sahilwaghere92@gmail.com>
2025-04-19 23:43:25 +0530

    Add files via upload

---
+++ backend/src/main/java/com/prioritcare/PrioritCareApplication.java
@@ -0,0 +1,11 @@
+package com.prioritcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioritCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrioritCareApplication.class, args);
+    }
+}
